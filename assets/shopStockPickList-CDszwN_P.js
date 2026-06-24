2ac0f6f537eda47dd4c167a5a5e07557bd68960c
dadaay1974 <daday1974@yahoo.com>
2026-06-23 21:17:22 -0500

    Updates

---
+++ assets/shopStockPickList-CDszwN_P.js
@@ -0,0 +1,2 @@
+import{L as e}from"./firestoreService-B3qE-gzP.js";function t(e){return e.split(/\r?\n/).map(e=>e.trim()).filter(e=>e.length>0)}function n(e){return e?.join(`
+`)??``}function r(t){return e(t)}function i(e,t){return`${e}:${t}`}export{i,r as n,t as r,n as t};
