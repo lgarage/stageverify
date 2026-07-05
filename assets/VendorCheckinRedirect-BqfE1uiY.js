0b187819bcb301fbe204a829c47dd99c44247feb
dadaay1974 <daday1974@yahoo.com>
2026-07-05 00:22:30 -0500

    Updates

---
+++ assets/VendorCheckinRedirect-BqfE1uiY.js
@@ -0,0 +1,1 @@
+import{Zt as e,en as t,qt as n}from"./index-1qRKOCMO.js";var r=n();function i(){let{orderId:n}=t(),i=n?.trim()??``;return(0,r.jsx)(e,{to:i?`/receive?id=${encodeURIComponent(i)}`:`/receive`,replace:!0})}export{i as CheckinToReceiveRedirect};
