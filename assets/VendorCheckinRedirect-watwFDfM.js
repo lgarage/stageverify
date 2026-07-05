cda43c5f6cc6bf6541558bd66cb333ad3ef75f4a
dadaay1974 <daday1974@yahoo.com>
2026-07-04 23:48:17 -0500

    Updates

--- assets/VendorCheckinRedirect-watwFDfM.js
+++
@@ -1,1 +0,0 @@
-import{Gt as e,Qt as t,Yt as n}from"./index-Ci0eiNJW.js";var r=e();function i(){let{orderId:e}=t(),i=e?.trim()??``;return(0,r.jsx)(n,{to:i?`/receive?id=${encodeURIComponent(i)}`:`/receive`,replace:!0})}export{i as CheckinToReceiveRedirect};
---
+++ assets/signOutWithConfirm-DH670IAJ.js
@@ -0,0 +1,1 @@
+import{Jt as e}from"./index-ClQrt2O5.js";function t(t,n){window.confirm(`Are you sure you want to sign out?`)&&e(t).then(()=>{n(`/login`,{replace:!0})})}export{t};
