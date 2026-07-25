8b5d91dda3cb273a54be98ca22fda856fbd577aa
dadaay1974 <daday1974@yahoo.com>
2026-07-25 09:55:04 -0500

    Updates

--- assets/InvoiceReviewPage-C8z3SuFx.js
+++
@@ -1,1 +0,0 @@
-import{Mr as e,xr as t}from"./index-DjnX2sGp.js";var n=t();function r(){return(0,n.jsx)(e,{to:`/dispatcher?focus=needs-review`,replace:!0})}export{r as InvoiceReviewPage};
--- assets/signOutWithConfirm-DgPwcOJt.js
+++
@@ -1,1 +0,0 @@
-import{Ar as e}from"./index-DjnX2sGp.js";function t(t,n){window.confirm(`Are you sure you want to sign out?`)&&e(t).then(()=>{n(`/login`,{replace:!0})})}export{t};
