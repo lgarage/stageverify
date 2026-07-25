edb4cc0876316ed6ae1e2a1282f38385d4e2d7e3
dadaay1974 <daday1974@yahoo.com>
2026-07-25 12:41:30 -0500

    Updates

---
+++ assets/VendorCheckinRedirect-Da10MUKb.js
@@ -0,0 +1,1 @@
+import{Ir as e,Nr as t,Sr as n}from"./index-B1plw2DS.js";var r=n();function i(){let{orderId:n}=e(),i=n?.trim()??``;return(0,r.jsx)(t,{to:i?`/receive?id=${encodeURIComponent(i)}`:`/receive`,replace:!0})}export{i as CheckinToReceiveRedirect};
