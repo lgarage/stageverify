ab4bd2f325415848a2d9fc31d5e04e19ce18a21b
dadaay1974 <daday1974@yahoo.com>
2026-08-01 18:58:55 -0500

    Updates

--- assets/signOutWithConfirm-BT_2Seoy.js
+++
@@ -1,1 +0,0 @@
-import{Jr as e}from"./index-RMi_4NBz.js";function t(t,n){window.confirm(`Are you sure you want to sign out?`)&&e(t).then(()=>{n(`/login`,{replace:!0})})}export{t};
---
+++ assets/signOutWithConfirm-DUxftnuO.js
@@ -0,0 +1,1 @@
+import{Jr as e}from"./index-B-j7KN1s.js";function t(t,n){window.confirm(`Are you sure you want to sign out?`)&&e(t).then(()=>{n(`/login`,{replace:!0})})}export{t};
