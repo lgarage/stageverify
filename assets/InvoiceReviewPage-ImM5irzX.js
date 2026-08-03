8b4bd8cb6f326df2e8c89bbb6e795fd2f2fbd799
dadaay1974 <daday1974@yahoo.com>
2026-08-03 18:22:23 -0500

    Updates

--- assets/InvoiceReviewPage-ImM5irzX.js
+++
@@ -1,1 +0,0 @@
-import{Ur as e,ei as t}from"./index-BlUXSam1.js";var n=e();function r(){return(0,n.jsx)(t,{to:`/dispatcher?focus=needs-review`,replace:!0})}export{r as InvoiceReviewPage};
--- assets/VendorCheckinRedirect-Ci7CA5L-.js
+++
@@ -1,1 +0,0 @@
-import{Ur as e,ei as t,ri as n}from"./index-BlUXSam1.js";var r=e();function i(){let{orderId:e}=n(),i=e?.trim()??``;return(0,r.jsx)(t,{to:i?`/receive?id=${encodeURIComponent(i)}`:`/receive`,replace:!0})}export{i as CheckinToReceiveRedirect};
